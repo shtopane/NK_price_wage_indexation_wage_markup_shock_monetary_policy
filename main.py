7eab99e1b678192e7178eb98809c9b35ece1c53d
shtopane <vechesumstudent@gmail.com>
2023-01-27 16:08:41 +0100

    Added ability to plot just a subset of variables - default is all variables

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ mod_dict0 = ep.parse(med_scale_nk_price_wage_indexation_file)
 
 shocks = ('e_w', 0.1)
 
-phi_y_list = [0.5, 1.0]
 phi_pi_list = [1., 2.0]
-psi_w_list = [120.60, 2000.0, 5250.0]
+psi_w_list = [2000.0, 5250.0]
+
+def get_subset_of_variable_indexes(mod_org):
+    want_to_show_vars_list = ['c', 'w', 'n', 'pi_w', 'pi', 'y', 'mc', 'R']
+    want_to_show_vars_indexes_list = []
+
+    for l in want_to_show_vars_list:
+        want_to_show_vars_indexes_list.append(mod_org['variables'].index(l))
+    
+    return want_to_show_vars_indexes_list
+
 def calculate_real_interest_rate(variables, x):
     result = None
     index_of_pi = variables.index('pi')
@@ -48,6 +57,12 @@ def solve_model_for_different_parameter_values(mod_dict_org, shocks, parameter_v
     
     # transform to real interest rate
     x_org1 = calculate_real_interest_rate(mod_org['variables'], x_org)
+    
+    # Select a subset of variables to plot
+    # Uncomment to show a subset of variables
+    # subset_variable_indexes = get_subset_of_variable_indexes(mod_org)
+    # subset_mod_org_stst = mod_org_stst[subset_variable_indexes]
+    
     models_tuple = ()
     
     for param_value in parameter_values_list:
@@ -62,20 +77,33 @@ def solve_model_for_different_parameter_values(mod_dict_org, shocks, parameter_v
         # transform to real interest rate
         x1 = calculate_real_interest_rate(mod_org['variables'], x)
         
+        # Uncomment line 83 and 86 to show just a subset of variables
+        # Select a subset of variables to show
+        # subset_x1 = x1[0:10, subset_variable_indexes]
+        
+        
+        # models_tuple = (*models_tuple, get_deviations_from_steady_state(subset_mod_org_stst, subset_x1))
         # Store IRFs
-        models_tuple = (*models_tuple, get_deviations_from_steady_state(mod_org_stst, x1[:30]))
+        models_tuple = (*models_tuple, get_deviations_from_steady_state(mod_org_stst, x1[:10]))
+        
     
-    figs, axs, _ = pplot((get_deviations_from_steady_state(mod_org_stst, x_org1[:30]), *models_tuple), labels = list(variables_names_map_sorted.values()))
+    # Plotting just a subset of variables. Uncomment lines 92. 93 and 95
+    # subset_x_org1 = x_org1[0:10, subset_variable_indexes]
+    # subset_labels = [list(variables_names_map_sorted.values())[i] for i in subset_variable_indexes]
     
-    # Add legend with values of the parameter(phi_y or phi_pi)
+    # figs, axs, _ = pplot((get_deviations_from_steady_state(subset_mod_org_stst, subset_x_org1), *models_tuple), labels = subset_labels)
+    figs, axs, _ = pplot((get_deviations_from_steady_state(mod_org_stst, x_org1[:10]), *models_tuple), labels = list(variables_names_map_sorted.values()))
+
+    
+    # Add legend with values of the parameter
+    # Add horizontal line at 0
     for ax in axs:
         ax.legend([f'{param_name} = {param_value}' for param_value in [param_value_org, *parameter_values_list]])
         ax.axhline(y=0.0, color='0.8', linestyle='--')
     
-    # for index, fig in enumerate(figs):
-    #     fig.savefig(f'plots/over_parameters/{param_name}/ten_periods/figure{index+1}.png')
     
     plt.show()
 
-# solve_model_for_different_parameter_values(mod_dict0, shocks, phi_y_list, 'phi_y')
+# Solve for either different values of phi_pi or psi_w
 solve_model_for_different_parameter_values(mod_dict0, shocks, phi_pi_list, 'phi_pi')
+# solve_model_for_different_parameter_values(mod_dict0, shocks, psi_w_list, 'psi_w')
